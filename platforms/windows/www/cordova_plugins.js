47a09535cfbf1aed80d2e0e08b3431a3a69f54fa
shaimuli <shaimuli@gmail.com>
2015-06-05 17:59:43 +0300

    dfg
    dfg

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,52 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowserProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/src/windows/NotificationProxy.js",
+        "id": "org.apache.cordova.dialogs.NotificationProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/src/windows/StatusBarProxy.js",
+        "id": "org.apache.cordova.statusbar.StatusBarProxy",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.statusbar": "0.1.10"
+}
+// BOTTOM OF METADATA
+});
