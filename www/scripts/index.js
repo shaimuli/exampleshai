384d0fb95601e5245db9095ef1762d812f49dad4
shaimuli <shaimuli@gmail.com>
2015-06-05 18:37:47 +0300

    hfgh
    fghfgh

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -8,22 +8,30 @@
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        alert("deviceready");
+        alert("onDeviceReady");
         var ref = window.open('http://www.jetcopy.co.il/', '_self', 'location=yes');
+        //ref.addEventListener('loadstart', function (event) { alert('start: ' + event.url); });
+        //ref.addEventListener('loadstop', function (event) { alert('stop: ' + event.url); });
+        //ref.addEventListener('loaderror', function (event) { alert('error: ' + event.message); });
+        //ref.addEventListener('exit', onBackKeyDown, false);
         ref.addEventListener("backbutton", onBackKeyDown, false);
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
+
     function onBackKeyDown(e) {
+        alert("onBackKeyDown");
         e.preventDefault();
         navigator.notification.confirm("אתה בטוח שברצונך לצאת?", onConfirm, "Confirmation", "Yes,No");
         // Prompt the user with the choice
     }
 
     function onConfirm(button) {
+        alert("onConfirm");
         if (button == 2) {//If User selected No, then we just do nothing
             return;
         } else {
             navigator.app.exitApp();// Otherwise we quit the app.
         }
     }
-
 } )();
